5d4bb779cfc539580583ae4e38d0b05b6f3c998a
Mm@@01147780927 <maboelayoun2@gmail.com>
2024-10-08 14:07:44 +0300

    using GetPost on postman and how get data whiches send in body then Deserialize data

--- firstNETApp/firstNETApp/Program.cs
+++ firstNETApp/firstNETApp/Program.cs
@@ -1,6 +1,24 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome in First NET Application");
+//app.MapGet("/", () => "Welcome in First NET Application");
+app.MapPost("/employees/add-employee", async(HttpContext httpcontext)=>{
+    //var key = httpcontext.Request.Headers["myKey"];
+    //await httpcontext.Response.WriteAsync($"Welcome Mahmoud Elwakel  {key}");
+    using (StreamReader stream = new StreamReader(httpcontext.Request.Body))
+    {
+        string body =await stream.ReadToEndAsync();
+        var response = JsonSerializer.Deserialize<Employee>(body);
+        await httpcontext.Response.WriteAsync($"{response?.name} {response?.age} {response?.gender}");
+    } 
+});
 
 app.Run();
+class Employee
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public string gender { get; set; }
+}
